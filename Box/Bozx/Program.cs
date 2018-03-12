66389aa641f35939d3cedf47259c68372c34159f
jcolongo <jagogardiner@gmail.com>
2018-03-12 12:26:54 +0000

    added box file

---
+++ Box/Bozx/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bozx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0; i < 100000000; i++)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("-   -");
+                Console.WriteLine("-   -");
+                Console.WriteLine("-----");
+                System.Threading.Thread.Sleep(500);
+                Console.Clear();
+                Console.WriteLine("     ");
+                Console.WriteLine("-----");
+                Console.WriteLine("-   -");
+                Console.WriteLine("-   -");
+                Console.WriteLine("-----");
+                System.Threading.Thread.Sleep(500);
+                Console.Clear();
+            }
+        }
+    }
+}
